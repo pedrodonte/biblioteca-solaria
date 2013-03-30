8e9a0ac186412c230905eba4a36a7d259ae3565e
Pedro Carrasco <pedrodonte@gmail.com>
2013-03-30 00:14:49 -0300

    son eliminados estos achivos porque se crearon copias dentro de solaria-project

--- solaria-portal/src/main/java/cl/jsoft/solaria/web/componentes/IBuscaCliente.java
+++
@@ -1,9 +0,0 @@
-package cl.jsoft.solaria.web.componentes;
-
-import cl.jsoft.solaria.dominio.vos.VoCliente;
-
-public interface IBuscaCliente {
-	
-	public void setClienteEncontrado(VoCliente cliente);
-
-}
